ee12d0d5d1ce4e80d42fcc41d56cd906b2febe44
small Bryan <laing0929@gmail.com>
2025-12-29 12:00:30 +0800

    chore: 更新 motor_control 套件配置
    - 新增 HS 和 Modbus 控制器入口點
    - 更新 launch 和 config 檔案清單
    - 新增 pyserial 依賴

--- src/motor_control/setup.py
+++ src/motor_control/setup.py
@@ -4,28 +4,36 @@ package_name = 'motor_control'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', [
-            'launch/keyboard_control.launch.py', 
+            'launch/keyboard_control.launch.py',
             'launch/esp32_motor_controller.launch.py',
+            'launch/modbus_motor_controller.launch.py',
+            'launch/hs_motor_controller.launch.py',
             'launch/full_system.launch.py',
             'launch/robot_state_publisher.launch.py'
         ]),
-        ('share/' + package_name + '/config', ['config/motor_config.yaml']),
+        ('share/' + package_name + '/config', [
+            'config/motor_config.yaml',
+            'config/modbus_motor_config.yaml',
+            'config/hs_motor_config.yaml'
+        ]),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'pyserial'],
     zip_safe=True,
     maintainer='jetson',
     maintainer_email='jetson@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Motor control package for differential drive robot',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'uart_send_to_esp32 = motor_control.uart_send_to_esp32:main'
+            'uart_send_to_esp32 = motor_control.uart_send_to_esp32:main',
+            'modbus_motor_controller = motor_control.modbus_motor_controller:main',
+            'hs_motor_controller = motor_control.hs_motor_controller:main'
         ],
     },
 )
